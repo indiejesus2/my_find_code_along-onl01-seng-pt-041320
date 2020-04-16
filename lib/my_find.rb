538dc5778768577e03d797d27c23942e8a95b755
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-16 04:26:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,6 +3,6 @@ require 'pry'
 def my_find(array)
   i = 0
   array.each do |collection|
-    return collection if yield(collection) else nil
+    return collection if yield(collection) else yield(nil)
   end
 end
